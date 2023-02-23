70937e7f846e2042b400e73bada2fec4fcbc3795
Dhoa0102 <hoa856856@gmail.com>
2023-02-23 14:02:52 +0700

    upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let song=document.getElementById("song");
+let ctr=document.getElementById("ctr").classList;
+song.onloadedmetadata = function() 
+{
+    lin.max=song.duration;
+    lin.value=song.currentTime;
+}
+
+function playPause()
+{
+    if(ctr.contains("fa-pause"))
+    {
+        song.pause();
+        ctr.remove("fa-pause");
+        ctr.add("fa-play");
+    }
+    else
+    {
+        song.play();
+        ctr.remove("fa-play");
+        ctr.add("fa-pause");
+    }
+}
+
